edf46cb3c0eb1d27b319ee8e90b8d36dc32fe5a9
Rishu0204 <rishuprasad220204@gmail.com>
2024-08-15 16:47:11 +0530

    Automated WebScraper

--- automated_AmazonWebScraper.py
+++ automated_AmazonWebScraper.py
@@ -7,12 +7,14 @@ import datetime
 
 def check():
     # List of URLs to scrape
+    #add more urls if needed
     URLS = [
         'https://www.amazon.in/gp/aw/d/B0CQPHMWR3/?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=6bab680f4c0692148df06644404f2dfd&hsa_cr_id=0&sr=1-3-undefined&ref_=sbx_be_dp_arbies_lsi4d_asin_2_bkgd&pd_rd_w=d4EdV&content-id=amzn1.sym.8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_p=8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_r=KD2AKQR2ZX1N6B2TE1WV&pd_rd_wg=rmw2y&pd_rd_r=ffc4158e-a65d-4ca2-a2dc-2b8b85e4e762&th=1',
         'https://www.amazon.in/gp/aw/d/B0CW3H8YKD/?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=250db33181732dc212a7561c2574bbd3&hsa_cr_id=0&sr=1-2-undefined&ref_=sbx_be_dp_arbies_lsi4d_asin_1_title&pd_rd_w=5cHpp&content-id=amzn1.sym.8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_p=8b13584a-c7fa-402c-b0a3-ddf39b989f1f&pf_rd_r=17BPXGHY5VK8EBBP1Y69&pd_rd_wg=Lp2x0&pd_rd_r=e6daee4a-5d9a-4512-8ecf-794244926e4d'
     ]
 
     # Headers to simulate a real browser visit
+    #get your user agent at: https://httpbin.org/get
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
         "Accept-Encoding": "gzip, deflate, br, zstd",
